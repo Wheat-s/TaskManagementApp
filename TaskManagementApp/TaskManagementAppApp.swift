f109ef5a0f04d0d777df85abd3d58c7035381586
Wheat <wheat8989@gmail.com>
2025-07-11 22:04:37 +0800

    Initial Commit

---
+++ TaskManagementApp/TaskManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementAppApp.swift
+//  TaskManagementApp
+//
+//  Created by wheat on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
